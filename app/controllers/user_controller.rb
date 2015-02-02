ae77c4ba54b900c46623d0f972a188bd68b2aada
Jordan Feldstein <jfeldstein@gmail.com>
2015-02-02 12:33:03 -0800

    require nilas token before showing dash

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,8 @@ class UserController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
+    return redirect_to nilas_connect_url unless current_user.nilas_token
+
     @nilasspace = nilas(current_user.nilas_token).namespaces.first
     @events     = @nilasspace.events
   end
