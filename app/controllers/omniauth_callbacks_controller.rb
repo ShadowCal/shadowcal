9b11d362d34c4120aa97146988780abc7b39a4f2
Jordan Feldstein <jfeldstein@gmail.com>
2017-10-20 16:19:12 -0700

    Better handling of existing users

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,11 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = current_user or User.find_or_create_from_omniauth(request.env["omniauth.auth"])
-
-    @user.add_or_update_google_account(request.env["omniauth.auth"])
+    @user = current_user
+    @user ||= User.find_or_create_from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      @user.add_or_update_google_account(request.env["omniauth.auth"])
+
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
