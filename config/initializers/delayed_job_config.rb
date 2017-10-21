e008b2df8619ed487f2509e2bc030f48e1146ff4
Jordan Feldstein <jfeldstein@gmail.com>
2017-10-21 16:17:03 -0700

    Only delay jobs in production

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,7 +4,7 @@ Delayed::Worker.sleep_delay = 5
 # Delayed::Worker.max_run_time = 5.minutes
 # Delayed::Worker.read_ahead = 10
 # Delayed::Worker.default_queue_name = 'default'
-Delayed::Worker.delay_jobs = !Rails.env.production?
+Delayed::Worker.delay_jobs = Rails.env.production?
 # Delayed::Worker.raise_signal_exceptions = :term
 
 # require 'delayed_job_log_setup'
