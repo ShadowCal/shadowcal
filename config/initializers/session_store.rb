8718fb23f129afef700509c9ad11594bb33f00a1
Jordan Feldstein <jfeldstein@gmail.com>
2017-10-22 21:51:19 -0700

    Fix cookies on prod

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,12 @@
 
 # Be sure to restart your server when you modify this file.
 
-ShadowCal::Application.config.session_store :cookie_store, key: "_ShadowCal_session", domain: :all
+if Rails.env.test?
+  ShadowCal::Application.config.session_store :cookie_store,
+                                              key: "_ShadowCal_session",
+                                              domain: :all
+else
+  ShadowCal::Application.config.session_store :cookie_store,
+                                              key: "_ShadowCal_session",
+                                              domain: 'shadowcal.com'
+end
