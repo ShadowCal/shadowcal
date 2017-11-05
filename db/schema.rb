0a912d96f1ddc4eaf5dbc93af5700cff232ed553
Jordan Feldstein <jfeldstein@gmail.com>
2017-11-04 20:14:17 -0700

    Add is_attending to Event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105025500) do
+ActiveRecord::Schema.define(version: 20171105031044) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20171105025500) do
     t.integer  "source_event_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "is_attending"
   end
 
   add_index "events", ["calendar_id"], name: "index_events_on_calendar_id"
