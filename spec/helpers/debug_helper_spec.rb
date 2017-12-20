235a7428394e4d5108f8221d143c446e2f285f01
Jordan Feldstein <jfeldstein@gmail.com>
2017-12-20 13:41:41 -0800

    Capture error w event missing start_at

--- spec/helpers/debug_helper_spec.rb
+++ spec/helpers/debug_helper_spec.rb
@@ -5,43 +5,51 @@ require "rails_helper"
 describe DebugHelper do
   describe "#identify_event" do
     subject { DebugHelper.identify_event(event) }
-    context "with new event" do 
-      let(:event) { build :event, name: "event.name"}
+    context "with new event" do
+      let(:event) { build :event, name: "event.name" }
 
-      it "says the name of the event" do 
+      it "says the name of the event" do
         expect(subject).to include("event.name")
       end
 
-      it "says 'new'" do 
+      it "says 'new'" do
         expect(subject).to include("new")
       end
     end
-    context "with saved event" do 
-      let(:event) { create :event, name: "event.name"}
+    context "with saved event" do
+      let(:event) { create :event, name: "event.name" }
 
-      it "says the name of the event" do 
+      it "says the name of the event" do
         expect(subject).to include("event.name")
       end
 
-      it "says the id" do 
-        expect(event.id).not_to be_nil #sanity
+      it "says the id" do
+        expect(event.id).not_to be_nil # sanity
         expect(subject).to include("##{event.id}")
       end
     end
 
-    context "with source event" do 
+    context "with event with no start_at" do
+      let(:event) { build :event, start_at: nil, end_at: nil }
+
+      it "doesn't crash" do
+        expect(subject).to include("Bad start_at")
+      end
+    end
+
+    context "with source event" do
       let(:event) { create :event, :has_shadow, name: "event.name" }
-      
-      it "says name of event" do 
+
+      it "says name of event" do
         expect(subject).to start_with('"event.name"')
       end
     end
 
-    context "with shadow event" do 
+    context "with shadow event" do
       let(:source_event) { create :event, :has_shadow, name: 'SourceEvent' }
       let(:event) { source_event.shadow_event }
 
-      it "says name of source event" do 
+      it "says name of source event" do
         expect(subject).to start_with('(Shadow of "SourceEvent"')
       end
     end
