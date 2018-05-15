0532ac32c27c32597600dc923544b8909b797389
Jordan Feldstein <jfeldstein@gmail.com>
2018-05-14 21:42:39 -0700

    Can add Outlook account

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,12 +6,27 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user ||= User.find_or_create_from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      @user.add_or_update_remote_account(request.env["omniauth.auth"], 'google_account')
+      @user.add_or_update_remote_account(request.env["omniauth.auth"], 'GoogleAccount')
 
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def microsoft_office365
+    @user = current_user
+    @user ||= User.find_or_create_from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      @user.add_or_update_remote_account(request.env["omniauth.auth"], 'OutlookAccount')
+
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Outlook"
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.outlook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
