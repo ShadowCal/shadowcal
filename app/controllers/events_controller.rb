70ee464dbdfd236c1a73dbe80152ebdbea44a688
Jordan Feldstein <jfeldstein@gmail.com>
2019-04-13 19:55:36 -0700

    Skip weekends

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,13 @@ class EventsController < ApplicationController
     @time_zone = @user.scheduling_calendar.time_zone
     @work_day_start = ZoneHelper.from_zoneless_timestamp_and_zone_to_utc('09:00:00', @time_zone)
     @work_day_end = ZoneHelper.from_zoneless_timestamp_and_zone_to_utc('17:00:00', @time_zone)
+
+    while weekend = @work_day_start.saturday? ||
+                    @work_day_start.sunday? do
+      @work_day_start += 1.day
+      @work_day_end += 1.day
+    end
+
     @interval = 30.minutes
     @first_day = @work_day_start.beginning_of_day
 
